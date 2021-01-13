59b501578ce952ff62747cf062e451643ee1cb00
Momshad Dinury <md.dinury@gmail.com>
2021-01-13 14:40:00 +0600

    padding changed to margin

--- lib/navigation_dot_bar.dart
+++ lib/navigation_dot_bar.dart
@@ -39,7 +39,7 @@ class _BottomNavigationDotBarState extends State<BottomNavigationDotBar> {
   @override
   Widget build(BuildContext context) => Container(
         color: Colors.transparent,
-        padding: EdgeInsets.fromLTRB(30, 0, 30, 10),
+        margin: EdgeInsets.fromLTRB(30, 0, 30, 10),
         child: Material(
             elevation: 5,
             borderRadius: BorderRadius.circular(20),
